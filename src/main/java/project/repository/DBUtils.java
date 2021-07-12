f5fda3d5ae7a7a605e5a990e98020dd75a9939da
Andrey <x137@yandex.ru>
2021-07-12 23:26:05 +0300

    12.07

--- src/main/java/project/repository/DBUtils.java
+++ src/main/java/project/repository/DBUtils.java
@@ -1,13 +1,68 @@
 package project.repository;
 
 
-import project.ConnectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 public class DBUtils {
+
+    private final Logger log = LoggerFactory.getLogger("project.DBUtils");
+    private static DBUtils INSTANCE = null;
+    private final Properties props;
+    private final Map<String, Object> context;
+
+    private DBUtils() {
+        Properties props = new Properties();
+        try (InputStream is = ClassLoader.getSystemResourceAsStream("application.properties")) {
+            props.load(is);
+        } catch (IOException e) {
+            log.warn("Unable to read application.properties");
+            System.err.println(e.getMessage());
+        }
+        this.props = props;
+        this.context = new HashMap<>();
+    }
+
+    public static DBUtils getInstance() {
+        if (INSTANCE == null) {
+            synchronized (DBUtils.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new DBUtils();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+
+    synchronized public Connection getConnection() {
+        Connection conn = (Connection) context.get("connection");
+        if (conn != null) {
+            return conn;
+        }
+        try {
+            conn = DriverManager.getConnection(props.getProperty("url"), props.getProperty("username"), props.getProperty("password"));
+        } catch (SQLException e) {
+            log.warn("IN - getConnection - Instance creation error Connection.");
+            e.printStackTrace();
+        }
+        log.info("IN - getConnection - Connection object created successfully.");
+        context.put("connection", conn);
+
+        return conn;
+    }
+
+    public Statement getStatementBegin() throws SQLException {
+        return getConnection().createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+    }
 
     public static String GET_LABEL_BY_ID = "SELECT `id`, `name` FROM labels WHERE id=%d";
 
@@ -57,7 +112,7 @@ public class DBUtils {
 
 
     public static Statement getStatement() throws SQLException {
-        return ConnectionUtils.getInstance()
-                .getStatement();
+        return DBUtils.getInstance()
+                .getStatementBegin();
     }
 }
